3c0468fa1f9c430bce185e78f100af7a5670019e
hongs98 <160989846+hongs98@users.noreply.github.com>
2025-07-13 18:18:29 +0900

    userDTO 컬럼 추가
    컬럼 추가

--- gitprac/src/main/java/com/ex/gitprac/data/user/UserDTO.java
+++ gitprac/src/main/java/com/ex/gitprac/data/user/UserDTO.java
@@ -9,8 +9,12 @@ public class UserDTO {
     private String id;              // 아이디
     private String pw;              // 패스워드
     private String nick;            // 닉네임
-    private LocalDateTime reg;      // 가입날짜
+    private String name;            // 이름
     private LocalDateTime birth;    // 생년월일
-    private int status;             // 상태
+    private LocalDateTime reg;      // 가입날짜
     private String email;           // 이메일
+    private String phone;           // 휴대폰
+    private String carrier;         // 통신사
+    private int status;             // 상태
+    private int role;               // 등급
 }
