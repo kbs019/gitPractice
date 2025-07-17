55b0a38e4fe5f2bcbcf50dce66f07aa406e7eba0
kr770d <kr770d@gmail.com>
2025-07-17 18:10:13 +0900

    rec 기본 틀
    controller, dto, mapper, service, xml 생성

---
+++ gitprac/src/main/java/com/ex/gitprac/controller/rec/RecController.java
@@ -0,0 +1,8 @@
+package com.ex.gitprac.controller.rec;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RecController {
+
+}
---
+++ gitprac/src/main/java/com/ex/gitprac/repository/rec/RecMapper.java
@@ -0,0 +1,8 @@
+package com.ex.gitprac.repository.rec;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class RecMapper {
+    
+}
