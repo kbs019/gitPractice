d516c62911c4c6b2d251389fe8f890e1b0e077b2
hongs98 <hongseckbum@gmail.com>
2025-07-18 13:19:54 +0900

    중간점검
    중간점검

--- gitprac/src/main/java/com/ex/gitprac/controller/rec/RecController.java
+++ gitprac/src/main/java/com/ex/gitprac/controller/rec/RecController.java
@@ -1,8 +1,13 @@
 package com.ex.gitprac.controller.rec;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequestMapping("/rec/*")
+@RequiredArgsConstructor
 public class RecController {
 
 }
