ce6fb3ad648f5b09c69f87412c456f958f3eb244
ItSSing <dbggoma@naver.com>
2025-07-18 18:17:42 +0900

    0718 diary 이미지가안올라가

--- gitprac/src/main/java/com/ex/gitprac/repository/diary/DiaryMapper.java
+++ gitprac/src/main/java/com/ex/gitprac/repository/diary/DiaryMapper.java
@@ -3,16 +3,17 @@ package com.ex.gitprac.repository.diary;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Param;
 
 import com.ex.gitprac.data.diary.DiaryDTO;
 
 @Mapper
-@Repository
 public interface DiaryMapper {
 
     public int insertDiary(DiaryDTO ddto);
     public int countDiary(String writer);
-    public List<DiaryDTO> listDiary(int start, int end, String writer);
-    public DiaryDTO contentDiary(int diaryNo);
+    public List<DiaryDTO> listDiary(@Param("writer") String writer, @Param("start") int start, @Param("end") int end);
+    public List<DiaryDTO> listDiaryByDate(@Param("writer") String writer, @Param("startDate") String startDate, @Param("endDate") String endDate, @Param("start") int start, @Param("end") int end);
+    public void deleteDiary(@Param("diaryNo") int diaryNo);
+    public void updateDiary(DiaryDTO dto);
 }
