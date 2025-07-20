ff6601ad2132bd61d33903193a09d61551e0816c
hongs98 <160989846+hongs98@users.noreply.github.com>
2025-07-21 01:29:37 +0900

    0721 01:30 아이디/비밀번호 찾기 삭제

--- gitprac/src/main/java/com/ex/gitprac/service/user/UserService.java
+++ gitprac/src/main/java/com/ex/gitprac/service/user/UserService.java
@@ -10,7 +10,6 @@ import com.ex.gitprac.repository.info.InfoBoardMapper;
 import com.ex.gitprac.repository.qna.QnaBoardMapper;
 import com.ex.gitprac.repository.qna.QnaReplyMapper;
 import com.ex.gitprac.repository.user.UserMapper;
-// import com.ex.gitprac.util.MailUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -70,31 +69,4 @@ public class UserService {
 
         return user;
     }
-    
-    // 아이디 찾기
-    public UserDTO findIdByEmail(String email) {
-        return userMapper.findIdByEmail(email);
-    }
-
-
-    // public String findPwAndSendTempPw( String id, String email ) {
-    //     UserDTO user = userMapper.findPwByIdEmail(id, email);
-    //     if( user != null ) {
-    //         String tempPW = generateTempPw();
-    //         userMapper.updatePw(id, email, tempPW);
-    //         MailUtil.sendTempPw(email, tempPW);
-    //         return tempPW;
-    //     }
-    //     return null;
-    // }
-
-    // private String generateTempPw() {
-    //     String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    //     StringBuilder sb = new StringBuilder();
-    //     for( int i = 0; i < 10; i++ ) { 
-    //         int idx = (int)(Math.random() * chars.length() );
-    //         sb.append(chars.charAt(idx));
-    //     }
-    //     return sb.toString();
-    // }
 }
