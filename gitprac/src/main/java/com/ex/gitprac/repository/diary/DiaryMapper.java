f499af49014f473b9a893dc80ff6c2acf28cbd05
ItSSing <dbggoma@naver.com>
2025-07-21 18:21:05 +0900

    다이어리에 달력 넣는중 집가서 함

--- gitprac/src/main/java/com/ex/gitprac/repository/diary/DiaryMapper.java
+++ gitprac/src/main/java/com/ex/gitprac/repository/diary/DiaryMapper.java
@@ -13,6 +13,7 @@ public interface DiaryMapper {
     public int insertDiary(DiaryDTO ddto);
     public int countDiary(String writer);
     public List<DiaryDTO> listDiary(@Param("writer") String writer, @Param("start") int start, @Param("end") int end);
+    public int countDiaryByDate(@Param("writer") String writer, @Param("startDate") String startDate, @Param("endDate") String endDate);
     public List<DiaryDTO> listDiaryByDate(@Param("writer") String writer, @Param("startDate") String startDate, @Param("endDate") String endDate, @Param("start") int start, @Param("end") int end);
     public void deleteDiary(@Param("diaryNo") int diaryNo);
     public void updateDiary(DiaryDTO dto);
